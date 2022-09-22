cae4b9b214a081215142b5f1400118730555eb61
Francisco <f.parrinha@fct.unl.pt>
2022-09-22 17:18:38 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import searchalgorithm.*;
+import searchproblem.*;
+import undirectedgraph.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Graph graph = Romenia.defineGraph();
+        graph.showLinks();
+        graph.showSets();
+        Node n;
+        n = graph.searchSolution("Timisoara", "Neamt", Algorithms.AStarSearch);
+        graph.showSolution(n);
+    }
+}
